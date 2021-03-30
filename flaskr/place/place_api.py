ee6895d914a001cbb3119510157c5662a235577e
seyeong <seyoung7555@naver.com>
2021-03-30 20:02:35 +0900

    날씨 api 연동

--- flaskr/place/place_api.py
+++ flaskr/place/place_api.py
@@ -4,6 +4,8 @@
 from flask import Blueprint, request, jsonify
 from flaskr import kakao_local
 from pprint import pprint
+from flaskr import location_code_fetcher
+from flaskr import weather_api
 bp = Blueprint('main', __name__, url_prefix='/')
 # @bp.route('/api', methods=('GET', 'POST'))
 @bp.route('/')
@@ -16,15 +18,21 @@ def search_place():
     if request.method == 'GET':
         kakao = kakao_local.kakao_local_api()
 
-        query = ['짜장면', '떡볶이', '양꼬치']
+        # query = ['짜장면', '떡볶이', '양꼬치']
         x = request.args.get('x')
         y = request.args.get('y')
         radius = int(request.args.get('radius'))
-        # print(x,y,radius)
+
+        grid_x, grid_y = location_code_fetcher.mapToGrid(float(y), float(x))
+        weather = weather_api.today_weather(str(grid_x), str(grid_y))
+
+
+        query = {"맑음" : ["치킨", "피자", "탕수육", "고기"]}
+
+
 
         res = []
-        for q in query:
-            # res.extend(kakao.search_keyword(q, x=x, y=y))
+        for q in query[weather]:
             res.extend(kakao.search_keyword(q, x=x, y=y, radius=radius, size=2)['documents'])
         # pprint(res)
         res = sorted(res, key=lambda x: int(x['distance']))
